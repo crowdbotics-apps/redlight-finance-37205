6a91b7222e83ac11e788df273a305e1ff04e3657
Crowdbotics <team@crowdbotics.com>
2022-10-03 13:53:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "redlight_finance_37205",
+  url: "https://redlight_finance_37205.botics.co",
+  api: "https://redlight_finance_37205.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
